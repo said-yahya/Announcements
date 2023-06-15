1895964a588a877d55c0d4d91cf4af9367dc5d34
ayub said <ayubayubsaid@gmail.com>
2023-06-15 17:49:53 +0500

    registered the two models and also added the perameters for Product panel of the administration

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+### for log-in into the administration panel 'username: admin', 'password: 123' ###
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'category', 'views', 'content', 'created_at', 'updated_at', 'publish')
+    list_display_links = ('title', )
+    list_editable = ('publish', )
+    readonly_fields = ('views', )
+
+admin.site.register(Category)
+admin.site.register(Product, ProductAdmin)
+
